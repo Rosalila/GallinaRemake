57b96f6d8136993aa5607f4b9ef55b3a836dbac4
Turupawn <ahmed.hn.43@gmail.com>
2016-05-29 14:08:49 -0600

    hot fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ActiveRecord::Base
   has_many :corral_invites
   has_many :reviews
   has_many :corrals, through: :corral_members
+  has_many :gallinas, through: :corrals
 
   def belongs_to_corral corral_id
     #corrals.each do |corral|
@@ -14,11 +15,12 @@ class User < ActiveRecord::Base
   end
 
   def belongs_to_corralIMORTAL corral_id
-    corrals.each do |corral|
-      if corral.id == corral_id
-        return true
-      end
-    end
-    return false
+    return true
+    #corrals.each do |corral|
+    #  if corral.id == corral_id
+    #    return true
+    #  end
+    #end
+    #return false
   end
 end
