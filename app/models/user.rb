5314d1e3ecacffcddb7c9279f38ba242ac21891e
Turupawn <ahmed.hn.43@gmail.com>
2016-07-26 16:37:01 -0600

    invite bug fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,12 +6,12 @@ class User < ActiveRecord::Base
   has_many :gallinas, through: :corrals
 
   def belongs_to_corral corral_id
-    #corrals.each do |corral|
-    #  if corral.id == corral_id
-    #    return true
-    #  end
-    #end
-    return true
+    corrals.each do |corral|
+      if corral.id == corral_id
+        return true
+      end
+    end
+    return false
   end
 
   def belongs_to_corralIMORTAL corral_id
